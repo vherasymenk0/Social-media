7f5476f27cd15b4b822a5cdba0ec8843562f4e8e
vherasymenko <manqarium@gmail.com>
2023-10-01 22:47:49 +0300

    [Fix] update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,32 @@
 /** @type {import('next').NextConfig} */
 
+function webpack(config) {
+  config.module.rules.push({
+    test: /\.svg$/i,
+    issuer: /\.[jt]sx?$/,
+    use: ['@svgr/webpack'],
+  })
+
+  return config
+}
+
 module.exports = {
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
-
-    return config
+  swcMinify: true,
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/random',
+      },
+    ],
+  },
+  webpack: webpack,
 }
