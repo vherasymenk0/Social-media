5f726b0118577b734448c1a73c5937def9ff5ae3
vherasymenko <manqarium@gmail.com>
2023-10-23 22:37:28 +0300

    [Refactor] ToasterContext to ToasterInitialazer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,15 @@
 import './globals.css'
 import { ReactNode } from 'react'
 
-import { ToasterProvider } from '~/contexts/Toaster.context'
+import ToasterInitializer from '~/services/toaster/ToasterInitializer'
 import ThemeRegistry from '~/theme/ThemeRegistry'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <ThemeRegistry>
-        <ToasterProvider>
-          <body>{children}</body>
-        </ToasterProvider>
+        <body>{children}</body>
+        <ToasterInitializer />
       </ThemeRegistry>
     </html>
   )
--- src/services/toaster/ToasterInitializer.tsx
+++ src/services/toaster/ToasterInitializer.tsx
@@ -1,14 +1,13 @@
 'use client'
-import React, { createContext, ReactNode, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { createPortal } from 'react-dom'
 
 import ToastNotification from '~/components/ToastNotification'
-import Toaster from '~/services/Toaster.service'
+import Toaster from '~/services/toaster/Toaster.service'
 import { Toast } from '~/types/shared'
+import { isBrowser } from '~/utils/helpers/util.helpers'
 
-const ToasterContext = createContext(null)
-
-export const ToasterProvider = ({ children }: { children: ReactNode }) => {
+const ToasterInitializer = () => {
   const [toast, setToast] = useState({} as Toast)
   const [isOpen, setIsOpen] = useState(false)
 
@@ -27,14 +26,15 @@ export const ToasterProvider = ({ children }: { children: ReactNode }) => {
     return () => Toaster.setGlobalToast(null)
   }, [])
 
+  if (!isBrowser()) return null
+
   return (
-    <ToasterContext.Provider value={null}>
-      {children}
-      {toast &&
-        createPortal(
-          <ToastNotification handleClose={handleClose} isOpen={isOpen} {...toast} />,
-          document.body
-        )}
-    </ToasterContext.Provider>
+    toast &&
+    createPortal(
+      <ToastNotification handleClose={handleClose} isOpen={isOpen} {...toast} />,
+      document.body
+    )
   )
 }
+
+export default ToasterInitializer
